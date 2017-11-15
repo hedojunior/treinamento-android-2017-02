afb9bc34a0bd0716d465895d68915f8eb82cf43f
Hedo Eccker da Silva Junior <hedo.junior@cwi.com.br>
2017-11-15 13:44:59 -0200

    initial commit.

---
+++ TesteLifecycle/app/src/main/java/com/hedo/myapplication/MainActivity.java
@@ -0,0 +1,66 @@
+package com.hedo.myapplication;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    private String tag = "TesteLifecycle";
+
+    private Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, Main2Activity.class);
+                startActivity(intent);
+            }
+        });
+        Log.i(tag, "MainActivity.onCreate");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i(tag, "MainActivity.onResume");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i(tag, "MainActivity.onStart");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i(tag, "MainActivity.onPause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i(tag, "MainActivity.onStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i(tag, "MainActivity.onDestroy");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.i(tag, "MainActivity.onRestart");
+    }
+}
